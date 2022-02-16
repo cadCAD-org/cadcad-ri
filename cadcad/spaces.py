aea0882c5b5f5c4d200b70566c4ab18a7eb2c5a9
Emanuel Lima <emanuel-lima@outlook.com>
2022-02-16 12:42:08 -0300

    Update Dimension with 100% test coverage

--- cadcad/spaces.py
+++ cadcad/spaces.py
@@ -1,16 +1,29 @@
 """Dimensions definition."""
 
 from dataclasses import dataclass
+from cadcad.utils import FreezingError
 
 
 @dataclass(eq=False)
 class Dimension():
-    """Dimensions in cadCAD."""
+    """Dimensions in cadCAD.
 
+    Attributes
+    ----------
+    dtype: type
+        type of the dimension
+    name : str
+        name of the dimension
+    description : str
+        optional description of the dimension
+    frozen : bool
+        wether the dimension is immutable or not
+    """
+
+    __dtype: type
     __name: str
-    __dtype: type  # should this be a union?
     __description: str = ""
-    __frozen: bool = False
+    _frozen: bool = False
 
     @property
     def name(self) -> str:
@@ -20,10 +33,10 @@ class Dimension():
     @name.setter
     def name(self, name: str) -> None:
         """Set Name."""
-        if not self.__frozen:
+        if not self._frozen:
             self.__name = name
         else:
-            raise Exception("Attempted to change a frozen Dimension.")
+            raise FreezingError
 
     @property
     def dtype(self) -> type:
@@ -33,10 +46,10 @@ class Dimension():
     @dtype.setter
     def dtype(self, dtype: type) -> None:
         """Set Dtype."""
-        if not self.__frozen:
+        if not self._frozen:
             self.__dtype = dtype
         else:
-            raise Exception("Attempted to change a frozen Dimension.")
+            raise FreezingError
 
     @property
     def description(self) -> str:
@@ -46,21 +59,21 @@ class Dimension():
     @description.setter
     def description(self, description: str) -> None:
         """Set Description."""
-        if not self.__frozen:
+        if not self._frozen:
             self.__description = description
         else:
-            raise Exception("Attempted to change a frozen Dimension.")
+            raise FreezingError
 
     def is_frozen(self) -> bool:
         """Get freezing status."""
-        return self.__frozen
+        return self._frozen
 
     def freeze(self) -> None:
         """Freeze the dimension to deny further changes to it.
 
         NOTE: This action is irreversible.
         """
-        self.__frozen = True
+        self._frozen = True
 
     def __eq__(self, other: object) -> bool:
         """Check if a dimension is equal to another.
@@ -75,8 +88,23 @@ class Dimension():
         if isinstance(other, Dimension):
             return self.dtype == other.dtype and self.name == other.name
 
-        return NotImplemented
+        raise NotImplementedError
 
     def __str__(self) -> str:
         """Return a string representation of a dimension."""
-        return f'Dimension {self.name} has data type {self.dtype}'
+        newline = '\n'
+
+        if not self._frozen and self.__description:
+            return (
+                f'Mutable dimension {self.name} has data type {self.dtype} '
+                f'and the following description: {newline}{self.description}{newline}'
+            )
+        elif not self._frozen and not self.__description:
+            return f'Mutable dimension {self.name} has data type {self.dtype} and no description'
+        elif self._frozen and not self.__description:
+            return f'Frozen dimension {self.name} has data type {self.dtype} and no description'
+        else:
+            return (
+                f'Frozen dimension {self.name} has data type {self.dtype} '
+                f'and the following description: {newline}{self.description}{newline}'
+            )
