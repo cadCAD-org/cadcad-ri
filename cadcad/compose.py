e476fbc7abd717e5c5545531798c45bc2956892c
Emanuel Lima <emlima@redhat.com>
2023-07-24 12:19:03 -0400

    WiP Composition rules
    Signed-off-by: Emanuel Lima <emlima@redhat.com>

---
+++ cadcad/compose.py
@@ -0,0 +1,152 @@
+"""Composition rules definitions."""
+
+from copy import deepcopy
+from functools import reduce
+from typing import Any, Collection
+
+from cadcad.dynamics import Block, block
+from cadcad.points import Point
+from cadcad.spaces import Space
+
+
+def serial_compose(block1: Block, block2: Block) -> Block:
+    """_summary_
+
+    Parameters
+    ----------
+    blocks : List[Block]
+        _description_
+
+    Returns
+    -------
+    Block
+        _description_
+    """
+    if not isinstance(block1, Block) or not isinstance(block2, Block):
+        raise TypeError("The arguments must be blocks")
+    if not block1.is_composable(block2):
+        raise ValueError("The blocks are not composable")
+
+    def new_function(*args: Any, **kwargs: Any) -> Point[Space] | Collection[Point[Space]]:
+        return block2.function(block1.function(*args, **kwargs))
+
+    annotation_dict = deepcopy(block1.function.__annotations__)
+    annotation_dict['return'] = block2.function.__annotations__['return']
+
+    #new_function.__annotations__.clear()
+    #new_function.__annotations__.update(annotation_dict)
+
+    setattr(new_function, "__annotations__", annotation_dict)
+
+
+    return block(new_function)
+
+def series(*blocks: Block) -> Block:
+    """_summary_
+
+    Parameters
+    ----------
+    blocks : List[Block]
+        _description_
+
+    Returns
+    -------
+    Block
+        _description_
+    """
+    if not blocks:
+        raise ValueError("At least one block must be provided")
+    return block(
+        reduce(
+            lambda f, g: serial_compose(f, g), blocks
+        )
+    )
+
+def parallel_compose(block1: Block, block2: Block) -> Block:
+    """_summary_
+
+    Parameters
+    ----------
+    blocks : List[Block]
+        _description_
+
+    Returns
+    -------
+    Block
+        _description_
+    """
+    if not isinstance(block1, Block) or not isinstance(block2, Block):
+        raise TypeError("The arguments must be blocks")
+    if not block1.is_composable(block2):
+        raise ValueError("The blocks are not composable")
+
+    # TODO: Parallel composition of blocks
+    def new_function(*args: Any, **kwargs: Any) -> Point[Space] | Collection[Point[Space]]:
+        return block1.function(*args, **kwargs), block2.function(*args, **kwargs)
+
+    annotation_dict = deepcopy(block1.function.__annotations__)
+    annotation_dict['return'] = block2.function.__annotations__['return']
+
+    new_function.__annotations__.clear()
+    new_function.__annotations__.update(annotation_dict)
+
+    return block(new_function)
+
+def parallel(*blocks: Block) -> Block:
+    """_summary_
+
+    Parameters
+    ----------
+    blocks : List[Block]
+        _description_
+
+    Returns
+    -------
+    Block
+        _description_
+    """
+    # TODO: Parallel composition of blocks
+    if not blocks:
+        raise ValueError("At least one block must be provided")
+    return block(
+        reduce(
+            lambda f, g: parallel_compose(f, g), blocks
+        )
+    )
+
+def stack_compose(block1: Block, block2: Block) -> Block:
+    """_summary_
+
+    Parameters
+    ----------
+    blocks : List[Block]
+        _description_
+
+    Returns
+    -------
+    Block
+        _description_
+    """
+    if not isinstance(block1, Block) or not isinstance(block2, Block):
+        raise TypeError("The arguments must be blocks")
+    if not block1.is_composable(block2):
+        raise ValueError("The blocks are not composable")
+    # TODO: Stack composition of blocks
+    return block()
+
+def stack(*blocks: Block) -> Block:
+    """_summary_
+
+    Returns
+    -------
+    Block
+        _description_
+    """
+    # TODO: Stack composition of blocks
+    if not blocks:
+        raise ValueError("At least one block must be provided")
+    return block(
+        reduce(
+            lambda f, g: parallel_compose(f, g), blocks
+        )
+    )
